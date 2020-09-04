f00004e973ab4fa2d1af2ec4496f37e1406b889c
mariobenissimo <>
2020-09-04 16:01:57 +0200

    little change

--- module4-solution/speakGoodBye.js
+++ module4-solution/speakGoodBye.js
@@ -1,6 +1,6 @@
 (function (window){
   var byeSpeaker = {};
-  var speakWord = "Hello";
+  var speakWord = "Good Bye";
   byeSpeaker.speak = function(name) {
     console.log(speakWord + " " + name);
   }
